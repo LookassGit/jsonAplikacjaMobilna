7db865706947c5b3ed965d0aa0ffae2e35b0f1fb
LookassGit <lukaszszkolakonto@gmail.com>
2026-09-25 12:17:11 +0200

    mainactivity

--- app/src/main/java/com/example/kostkikamilpoplawski4pt/MainActivity.java
+++ app/src/main/java/com/example/kostkikamilpoplawski4pt/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.kostkikamilpoplawski4pt;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Pytanie> pytania;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +31,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://my-json-server.typicode.com/LookassGit/json/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+    JsonPlaceholderApi jsonPlaceholderApi = retrofit.create(JsonPlaceholderApi.class);
+
+    Call<ArrayList<Pytanie>> call = jsonPlaceholderApi.getPytania();
+
+    call.enqueue(
+            new Callback<ArrayList<Pytanie>>() {
+                @Override
+                public void onResponse(Call<ArrayList<Pytanie>> call, Response<ArrayList<Pytanie>> response) {
+                    if(!response.isSuccessful()){
+                        Toast.makeText(MainActivity.this, response.code(), Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    pytania = response.body();
+                    Toast.makeText(MainActivity.this, pytania.get(0).tresc, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(Call<ArrayList<Pytanie>> call, Throwable t) {
+
+                }
+            }
+    );
     }
 }
 //https://my-json-server.typicode.com/mechaniktgmobilne/pytania_retrofit
